7f541780e23b2c2e19e0d3599c9f4df7c3f1ab60
hitman281291 <hitesh.purohit28@gmail.com>
2025-03-13 07:36:48 +0530

    Lab001-File-Added

---
+++ src/ex_05_TypeCasting/Lab057_Typecasting_0.java
@@ -0,0 +1,16 @@
+package ex_05_TypeCasting;
+
+public class Lab057_Typecasting_0 {
+
+    public static void main(String[] args) {
+        byte b =10;
+
+      //  int a = b;
+
+        int a = (int) b;
+
+    }
+
+}
+
+
