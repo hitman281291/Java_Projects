5b9065b95f1623233a387104b52e2b6f47ea03aa
hitman281291 <hitesh.purohit28@gmail.com>
2025-03-31 21:18:51 +0530

    Lab001-File-Added

---
+++ src/ex_17_OOPs/ATB11x.java
@@ -0,0 +1,21 @@
+package ex_17_OOPs;
+
+public class ATB11x {
+    // Attributes
+    String name;
+    int age;
+    String course;
+    String email;
+    int salary;
+    String gender;
+
+
+
+    // Behaviour
+    void speak(){}
+    void doAssignment(){}
+    void doTask(){}
+    void askQuestion(){}
+    void learnCoding(){}
+
+}
