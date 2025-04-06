e4781ee678634e3732dce4461b6d743942ff4eb7
hitman281291 <hitesh.purohit28@gmail.com>
2025-04-06 21:19:52 +0530

    updated program

--- src/ex_06_Ternary_Operator/Lab067_TO_Three_Max.java
+++ src/ex_06_Ternary_Operator/Lab067_TO_Three_Max.java
@@ -1,27 +1,27 @@
 package ex_06_Ternary_Operator;
 
-public class Lab067_TO_Three_Max
-{
+public class Lab067_To_Three_Max {
     public static void main(String[] args) {
-
         int n1 = 2;
         int n2 = 9;
         int n3 = -11;
 
-        //Logic building formula
+        // Logic Building formula
 
-        //Step 1 : Find inputs/outputs
-        //I/O --> n1, m2, n3 - int
-        //O/P --> String -> Max number
+        // Step 1 : Find inputs /outputs
+        // I/O -> n1,n2,n3 - int
+        // O/P -> String ->  max number
 
-        //Step 2 - Rough Logic
-        //n1 > n2 and n1 > n3 -->
-        //n2 > n3 and n2 > n1 -> n2
-        //n3
+        // Step 2 - Rough Logic, Think about it.
+        // n1 > n2 and n1  > n3 -> n1
+        // n2 > n3 and n2>  n1 ->  n2
+        // n3
 
-        int max = (n1>n2) ? (n1 > n3) ? n1 : n3 : ((n2 > n3) ? n2 : n3);
-        System.out.println("Max is : "+max);
+        int max = (n1 > n2) ? (n1 > n3) ? n1 : n3 : ((n2 > n3) ? n2 : n3);
+        System.out.println("Max ix " + max);
+
+        // A -> (n1 > n3) ? n1 : n3
+
 
     }
-
 }
